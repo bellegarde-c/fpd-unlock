9128ff6f3ab22596da347410a1444c871cd428f8
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-05-22 18:23:16 +0200

    Use available signals to check auth

--- src/fpd-unlock.c
+++ src/fpd-unlock.c
@@ -27,9 +27,6 @@
 #define FEEDBACKD_DBUS_PATH           "/org/sigxcpu/Feedback"
 #define FEEDBACKD_DBUS_INTERFACE      "org.sigxcpu.Feedback"
 
-#define UNLOCK_TIMEOUT    1000
-
-
 struct _FpdUnlockPrivate {
     GDBusProxy *logind_manager_proxy;
     GDBusProxy *logind_session_proxy;
@@ -40,7 +37,6 @@ struct _FpdUnlockPrivate {
     const gchar *session_path;
     gboolean idle_hint;
     gboolean locked_hint;
-    guint timeout_id;
 };
 
 G_DEFINE_TYPE_WITH_CODE (
@@ -92,14 +88,11 @@ fpd_unlock_do (FpdUnlock *self)
     }
 }
 
-static gboolean
-fpd_unlock_handle_unlocking (FpdUnlock *self)
+static void
+fpd_unlock_start_unlocking (FpdUnlock *self)
 {
     g_autoptr(GError) error = NULL;
     g_autoptr(GVariant) identify = NULL;
-    gint result;
-
-    self->priv->timeout_id = 0;
 
     identify = g_dbus_proxy_call_sync (
         self->priv->fingerprint_proxy,
@@ -112,23 +105,29 @@ fpd_unlock_handle_unlocking (FpdUnlock *self)
     );
 
     if (error != NULL) {
-        g_warning ("Can't identify fingerprint: %s", error->message);
-        return FALSE;
+        g_warning ("Can't start fingerprint auth: %s", error->message);
     }
+}
 
-    g_variant_get (identify, "(i)", &result);
-    if (result == 0) {
-        fpd_unlock_feedback (self, "button-pressed");
-        fpd_unlock_do (self);
-    } else {
-        self->priv->timeout_id = g_timeout_add (
-            UNLOCK_TIMEOUT,
-            (GSourceFunc) fpd_unlock_handle_unlocking,
-            self
-        );
+static void
+fpd_unlock_stop_unlocking (FpdUnlock *self)
+{
+    g_autoptr(GError) error = NULL;
+    g_autoptr(GVariant) abort = NULL;
+
+    abort = g_dbus_proxy_call_sync (
+        self->priv->fingerprint_proxy,
+        "Abort",
+        NULL,
+        G_DBUS_CALL_FLAGS_NONE,
+        -1,
+        NULL,
+        &error
+    );
+
+    if (error != NULL) {
+        g_warning ("Can't stop fingerprint auth: %s", error->message);
     }
-
-    return FALSE;
 }
 
 static void
@@ -162,12 +161,42 @@ fpd_get_active_session (FpdUnlock *self)
 }
 
 static void
-fpd_logind_proxy_seat_properties_cb (GDBusProxy *proxy,
-                                     GVariant   *changed_properties,
-                                     char      **invalidated_properties,
-                                     gpointer    user_data)
+on_fingerprint_proxy_signal (GDBusProxy  *proxy,
+                             const gchar *sender_name,
+                             const gchar *signal_name,
+                             GVariant    *parameters,
+                             gpointer     user_data)
 {
-    FpdUnlock *self = user_data;
+    FpdUnlock *self = FPD_UNLOCK (user_data);
+
+    if (g_strcmp0 (signal_name, "Identified") == 0) {
+        const gchar *finger;
+
+        g_variant_get (parameters, "(&s)", &finger);
+
+        g_message ("Auth by: %s", finger);
+        fpd_unlock_do (self);
+    } else if (g_strcmp0 (signal_name, "ErrorInfo") == 0) {
+        fpd_unlock_feedback (self, "bell-terminal");
+    } else if (g_strcmp0 (signal_name, "StateChanged") == 0) {
+        const gchar *state;
+
+        g_variant_get (parameters, "(&s)", &state);
+        if (g_strcmp0 (state, "FPSTATE_IDLE") == 0) {
+            if (self->priv->idle_hint && self->priv->locked_hint)
+                fpd_unlock_start_unlocking (self);
+        }
+    }
+
+}
+
+static void
+on_logind_proxy_seat_properties_changed (GDBusProxy *proxy,
+                                         GVariant   *changed_properties,
+                                         char      **invalidated_properties,
+                                         gpointer    user_data)
+{
+    FpdUnlock *self = FPD_UNLOCK (user_data);
     GVariant *value;
     GVariantIter i;
     gchar *property;
@@ -175,17 +204,15 @@ fpd_logind_proxy_seat_properties_cb (GDBusProxy *proxy,
     g_variant_iter_init (&i, changed_properties);
     while (g_variant_iter_next (&i, "{&sv}", &property, &value)) {
         if (g_strcmp0 (property, "IdleHint") == 0) {
-            g_clear_handle_id (&self->priv->timeout_id, g_source_remove);
             self->priv->idle_hint = g_variant_get_boolean (value);
-            if (!self->priv->idle_hint)
-                self->priv->timeout_id = g_timeout_add (
-                    UNLOCK_TIMEOUT,
-                    (GSourceFunc) fpd_unlock_handle_unlocking,
-                    self
-                );
+            if (self->priv->idle_hint)
+                fpd_unlock_stop_unlocking (self);
+            else
+                fpd_unlock_start_unlocking (self);
         } else if (g_strcmp0 (property, "LockedHint") == 0) {
-            g_clear_handle_id (&self->priv->timeout_id, g_source_remove);
             self->priv->locked_hint = g_variant_get_boolean (value);
+            if (!self->priv->locked_hint)
+                fpd_unlock_stop_unlocking (self);
         }
         g_variant_unref (value);
     }
@@ -195,8 +222,6 @@ static void
 fpd_unlock_dispose (GObject *fpd_unlock)
 {
     FpdUnlock *self = FPD_UNLOCK (fpd_unlock);
-
-    g_clear_handle_id (&self->priv->timeout_id, g_source_remove);
 
     g_clear_object (&self->priv->logind_session_proxy);
     g_clear_object (&self->priv->logind_manager_proxy);
@@ -232,8 +257,6 @@ fpd_unlock_init (FpdUnlock *self)
     g_autoptr(GError) error = NULL;
 
     self->priv = fpd_unlock_get_instance_private (self);
-
-    self->priv->timeout_id = 0;
 
     self->priv->logind_manager_proxy =
                 g_dbus_proxy_new_for_bus_sync (
@@ -292,9 +315,16 @@ fpd_unlock_init (FpdUnlock *self)
                );
 
     g_signal_connect (
+        self->priv->fingerprint_proxy,
+        "g-signal",
+        G_CALLBACK (on_fingerprint_proxy_signal),
+        self
+    );
+
+    g_signal_connect (
         self->priv->logind_session_proxy,
         "g-properties-changed",
-        G_CALLBACK (fpd_logind_proxy_seat_properties_cb),
+        G_CALLBACK (on_logind_proxy_seat_properties_changed),
         self
     );
 }
