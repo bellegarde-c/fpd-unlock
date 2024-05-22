2611a7fd21556a621216a4b9ff44ec703763fe6e
Cédric Bellegarde <cedric.bellegarde@adishatz.org>
2024-05-22 17:06:02 +0200

    Make unlock working

--- src/fpd-unlock.c
+++ src/fpd-unlock.c
@@ -7,29 +7,38 @@
 
 #include <gio/gio.h>
 
+#include "config.h"
 #include "fpd-unlock.h"
 
-#define DBUS_PROPERTIES              "org.freedesktop.DBus.Properties"
+#define DBUS_PROPERTIES               "org.freedesktop.DBus.Properties"
 
-#define LOGIND_DBUS_NAME             "org.freedesktop.login1"
-#define LOGIND_DBUS_SEAT_PATH        "/org/freedesktop/login1/session/auto"
-#define LOGIND_DBUS_SEAT_INTERFACE   "org.freedesktop.login1.Session"
-#define LOGIND_DBUS_UNLOCK_PATH      "/org/freedesktop/login1"
-#define LOGIND_DBUS_UNLOCK_INTERFACE "org.freedesktop.login1.Manager"
+#define LOGIND_DBUS_NAME              "org.freedesktop.login1"
 
-#define FINGERPRINT_DBUS_NAME        "org.droidian.fingerprint"
-#define FINGERPRINT_DBUS_PATH        "/org/droidian/fingerprint"
-#define FINGERPRINT_DBUS_INTERFACE   "org.droidian.fingerprint"
+#define LOGIND_DBUS_SESSION_INTERFACE "org.freedesktop.login1.Session"
 
-#define UNLOCK_RATE 250
+#define LOGIND_DBUS_MANAGER_PATH      "/org/freedesktop/login1"
+#define LOGIND_DBUS_MANAGER_INTERFACE "org.freedesktop.login1.Manager"
+
+#define FINGERPRINT_DBUS_NAME         "org.droidian.fingerprint"
+#define FINGERPRINT_DBUS_PATH         "/org/droidian/fingerprint"
+#define FINGERPRINT_DBUS_INTERFACE    "org.droidian.fingerprint"
+
+#define FEEDBACKD_DBUS_NAME           "org.sigxcpu.Feedback"
+#define FEEDBACKD_DBUS_PATH           "/org/sigxcpu/Feedback"
+#define FEEDBACKD_DBUS_INTERFACE      "org.sigxcpu.Feedback"
+
+#define SCREEN_ON_TIMEOUT 750
+#define UNLOCK_TIMEOUT    250
 
 
 struct _FpdUnlockPrivate {
-    GDBusProxy *logind_seat_proxy;
     GDBusProxy *logind_manager_proxy;
+    GDBusProxy *logind_session_proxy;
     GDBusProxy *fingerprint_proxy;
+    GDBusProxy *feedbackd_proxy;
 
     const gchar *session_id;
+    const gchar *session_path;
     gboolean idle_hint;
     gboolean locked_hint;
     guint timeout_id;
@@ -43,14 +52,36 @@ G_DEFINE_TYPE_WITH_CODE (
 )
 
 static void
+fpd_unlock_feedback (FpdUnlock *self, const gchar *event)
+{
+    g_autoptr(GError) error = NULL;
+
+    g_return_if_fail (self->priv->feedbackd_proxy != NULL);
+
+    g_dbus_proxy_call_sync (
+        self->priv->feedbackd_proxy,
+        "TriggerFeedback",
+        g_variant_new ("(&s&sa{sv}i)", APP_ID, event, NULL, -1),
+        G_DBUS_CALL_FLAGS_NONE,
+        -1,
+        NULL,
+        &error
+    );
+
+    if (error != NULL) {
+        g_warning ("Can't send feedback: %s", error->message);
+    }
+}
+
+static void
 fpd_unlock_do (FpdUnlock *self)
 {
     g_autoptr(GError) error = NULL;
 
     g_dbus_proxy_call_sync (
-        self->priv->fingerprint_proxy,
+        self->priv->logind_manager_proxy,
         "UnlockSession",
-        NULL,
+        g_variant_new ("(&s)", self->priv->session_id),
         G_DBUS_CALL_FLAGS_NONE,
         -1,
         NULL,
@@ -69,6 +100,8 @@ fpd_unlock_handle_unlocking (FpdUnlock *self)
     g_autoptr(GVariant) identify = NULL;
     gint result;
 
+    self->priv->timeout_id = 0;
+
     identify = g_dbus_proxy_call_sync (
         self->priv->fingerprint_proxy,
         "Identify",
@@ -81,15 +114,16 @@ fpd_unlock_handle_unlocking (FpdUnlock *self)
 
     if (error != NULL) {
         g_warning ("Can't identify fingerprint: %s", error->message);
+        return FALSE;
     }
 
     g_variant_get (identify, "(i)", &result);
-
     if (result == 0) {
+        fpd_unlock_feedback (self, "button-pressed");
         fpd_unlock_do (self);
     } else {
-        self->priv->timeout_id = g_timeout_add_seconds (
-            UNLOCK_RATE,
+        self->priv->timeout_id = g_timeout_add (
+            UNLOCK_TIMEOUT,
             (GSourceFunc) fpd_unlock_handle_unlocking,
             self
         );
@@ -103,12 +137,13 @@ fpd_get_active_session (FpdUnlock *self)
 {
     g_autoptr(GError) error = NULL;
     g_autoptr(GVariant) value = NULL;
-    g_autoptr(GVariant) session_id = NULL;
+    g_autoptr (GVariantIter) iter;
+    const gchar *seat;
 
     value = g_dbus_proxy_call_sync (
-        self->priv->logind_seat_proxy,
-        "Get",
-        g_variant_new ("(&ss)", LOGIND_DBUS_SEAT_INTERFACE, "Id"),
+        self->priv->logind_manager_proxy,
+        "ListSessions",
+        NULL,
         G_DBUS_CALL_FLAGS_NONE,
         -1,
         NULL,
@@ -119,58 +154,12 @@ fpd_get_active_session (FpdUnlock *self)
         g_warning ("Can't get active session id: %s", error->message);
     }
 
-    g_variant_get (value, "(v)", &session_id);
-    self->priv->session_id = g_strdup (g_variant_get_string (session_id, NULL));
-}
-
-static void
-fpd_get_idle_hint (FpdUnlock *self)
-{
-    g_autoptr(GError) error = NULL;
-    g_autoptr(GVariant) value = NULL;
-    g_autoptr(GVariant) idle_hint = NULL;
-
-    value = g_dbus_proxy_call_sync (
-        self->priv->logind_seat_proxy,
-        "Get",
-        g_variant_new ("(&ss)", LOGIND_DBUS_SEAT_INTERFACE, "IdleHint"),
-        G_DBUS_CALL_FLAGS_NONE,
-        -1,
-        NULL,
-        &error
-    );
-
-    if (error != NULL) {
-        g_warning ("Can't get active session id: %s", error->message);
+    g_variant_get (value, "(a(susso))", &iter);
+    while (g_variant_iter_loop (iter, "(susso)", &self->priv->session_id,
+                                NULL, NULL, &seat, &self->priv->session_path)) {
+        if (g_strcmp0 (seat, "seat0") == 0)
+            break;
     }
-
-    g_variant_get (value, "(v)", &idle_hint);
-    self->priv->idle_hint = g_variant_get_boolean(idle_hint);
-}
-
-static void
-fpd_get_locked_hint (FpdUnlock *self)
-{
-    g_autoptr(GError) error = NULL;
-    g_autoptr(GVariant) value = NULL;
-    g_autoptr(GVariant) locked_hint = NULL;
-
-    value = g_dbus_proxy_call_sync (
-        self->priv->logind_seat_proxy,
-        "Get",
-        g_variant_new ("(&ss)", LOGIND_DBUS_SEAT_INTERFACE, "LockedHint"),
-        G_DBUS_CALL_FLAGS_NONE,
-        -1,
-        NULL,
-        &error
-    );
-
-    if (error != NULL) {
-        g_warning ("Can't get active session id: %s", error->message);
-    }
-
-    g_variant_get (value, "(v)", &locked_hint);
-    self->priv->locked_hint = g_variant_get_boolean(locked_hint);
 }
 
 static void
@@ -189,9 +178,12 @@ fpd_logind_proxy_seat_properties_cb (GDBusProxy *proxy,
         if (g_strcmp0 (property, "IdleHint") == 0) {
             g_clear_handle_id (&self->priv->timeout_id, g_source_remove);
             self->priv->idle_hint = g_variant_get_boolean (value);
-
             if (!self->priv->idle_hint)
-                fpd_unlock_handle_unlocking (self);
+                self->priv->timeout_id = g_timeout_add (
+                    SCREEN_ON_TIMEOUT,
+                    (GSourceFunc) fpd_unlock_handle_unlocking,
+                    self
+                );
         } else if (g_strcmp0 (property, "LockedHint") == 0) {
             g_clear_handle_id (&self->priv->timeout_id, g_source_remove);
             self->priv->locked_hint = g_variant_get_boolean (value);
@@ -207,9 +199,10 @@ fpd_unlock_dispose (GObject *fpd_unlock)
 
     g_clear_handle_id (&self->priv->timeout_id, g_source_remove);
 
-    g_clear_object (&self->priv->logind_seat_proxy);
+    g_clear_object (&self->priv->logind_session_proxy);
     g_clear_object (&self->priv->logind_manager_proxy);
     g_clear_object (&self->priv->fingerprint_proxy);
+    g_clear_object (&self->priv->feedbackd_proxy);
 
     G_OBJECT_CLASS (fpd_unlock_parent_class)->dispose (fpd_unlock);
 }
@@ -243,28 +236,14 @@ fpd_unlock_init (FpdUnlock *self)
 
     self->priv->timeout_id = 0;
 
-    self->priv->logind_seat_proxy =
-                g_dbus_proxy_new_for_bus_sync (
-                    G_BUS_TYPE_SYSTEM,
-                    0,
-                    NULL,
-                    LOGIND_DBUS_NAME,
-                    LOGIND_DBUS_SEAT_PATH,
-                    DBUS_PROPERTIES,
-                    NULL,
-                    NULL
-               );
-
-    g_assert (self->priv->logind_seat_proxy != NULL);
-
     self->priv->logind_manager_proxy =
                 g_dbus_proxy_new_for_bus_sync (
                     G_BUS_TYPE_SYSTEM,
                     0,
                     NULL,
                     LOGIND_DBUS_NAME,
-                    LOGIND_DBUS_UNLOCK_PATH,
-                    LOGIND_DBUS_UNLOCK_INTERFACE,
+                    LOGIND_DBUS_MANAGER_PATH,
+                    LOGIND_DBUS_MANAGER_INTERFACE,
                     NULL,
                     NULL
                );
@@ -285,16 +264,40 @@ fpd_unlock_init (FpdUnlock *self)
 
     g_assert (self->priv->fingerprint_proxy != NULL);
 
+    fpd_get_active_session (self);
+
+    self->priv->logind_session_proxy =
+                g_dbus_proxy_new_for_bus_sync (
+                    G_BUS_TYPE_SYSTEM,
+                    0,
+                    NULL,
+                    LOGIND_DBUS_NAME,
+                    self->priv->session_path,
+                    LOGIND_DBUS_SESSION_INTERFACE,
+                    NULL,
+                    NULL
+               );
+
+    g_assert (self->priv->logind_session_proxy != NULL);
+
+    self->priv->feedbackd_proxy =
+                g_dbus_proxy_new_for_bus_sync (
+                    G_BUS_TYPE_SESSION,
+                    0,
+                    NULL,
+                    FEEDBACKD_DBUS_NAME,
+                    FEEDBACKD_DBUS_PATH,
+                    FEEDBACKD_DBUS_INTERFACE,
+                    NULL,
+                    NULL
+               );
+
     g_signal_connect (
-        self->priv->logind_seat_proxy,
+        self->priv->logind_session_proxy,
         "g-properties-changed",
         G_CALLBACK (fpd_logind_proxy_seat_properties_cb),
         self
     );
-
-    fpd_get_locked_hint (self);
-    fpd_get_idle_hint (self);
-    fpd_get_active_session (self);
 }
 
 
